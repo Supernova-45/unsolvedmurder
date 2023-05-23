cfb82132d839e58c0ae2c7fae5c53571d63db1c1
Alexandra Kim <alexandrasuriya@gmail.com>
2023-05-23 14:58:06 -0700

    Added bare bones code

---
+++ js/commands.js
@@ -0,0 +1,76 @@
+var curius = "https://curius.app/linda2";
+var bookshelf = "https://bookshelf.website/lkt/";
+var twitter = "https://www.twitter.com/lktong_/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/lindaktong/";
+var goodreads = "https://www.goodreads.com/user/show/45442005-lt/";
+var github = "https://github.com/lindaktong/";
+var email = 'mailto:lkt@stanford.edu';
+
+whois = [
+  "<br>",
+  "hey, i'm linda‹",
+  "i study cs & classics at stanford",
+  "in my free time, i like reading books and longform articles, going on walks, and making things",
+  "i'm also a fellow @pearvc! reach out if you're starting something new or interested in learning more",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of â€œWho am I?â€ is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'curius         <a href="' + curius + '" target="_blank">curius/linda2' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/lktong_' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/lindaktong' + "</a>",
+  'goodreads      <a href="' + goodreads + '" target="_blank">goodreads/user/show/45442005-lt' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/lindaktong' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "still curating! most projects are offline or on github.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          hi there!',
+  '<span class="command">whoami</span>         who are you?',
+  '<span class="command">bookshelf</span>      hey mtv, welcome to my lib',
+  '<span class="command">social</span>         catch me around the web',
+  '<span class="command">secret</span>         find the password',
+  '<span class="command">projects</span>       view coding projects',
+  '<span class="command">history</span>        view command history',
+  '<span class="command">help</span>           (you\'re here)',
+  '<span class="command">email</span>          say hi! i don\'t bite :)',
+  '<span class="command">clear</span>          clear terminal',
+  '<span class="command">banner</span>         display the header',
+  "<br>",
+];
+
+banner = [
+"    __          ,_ _",                          
+"    / ()  ()    /| | |         ,_   _|   _  ,_",
+"   |      /\     | | |  |  |  /  | / |  |/ /  |",
+"    \___//(_)    | | |_/ \/|_/   |/\/|_/|_/   |/",
+"   (|   |         ,   _  |\       _  _| ",        
+"    |   |  /|/|  / \_/ \_|/ |  |_|/ / | ",        
+"     \_/\_/ | |_/ \/ \_/ |_/ \/  |_/\/|_/ ",  
+"   ",   
+                                                
+  '<span class="color2">hello world, are you ready to solve a CS class mystery?</span>',
+  "<span class=\"color2\">to start, type</span> <span class=\"command\">'explore'</span><span class=\"color2\">.</span>",
+  "         "
+];
